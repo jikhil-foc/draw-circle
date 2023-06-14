aff71fb3a312f054332ef98b3c604b4427930770
Jikhil Ps <jikhilps@focaloid.com>
2023-06-14 11:46:42 +0530

    text label added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
   ngAfterViewInit(): void {
     const parentDiv = document.createElement('div');
 
-    [1, 2, 3, 4].forEach(() => {
+    [1].forEach(() => {
       const data = this.drawDefectService.generateImage();
       const imageDiv = document.createElement('img');
       imageDiv.src = data;
@@ -19,116 +19,5 @@ export class AppComponent {
     });
 
     document.body.appendChild(parentDiv);
-  }
-
-  generateImage(): void {
-    const canvas = document.createElement('canvas');
-    canvas.width = 900; // Adjust the dimensions as per your requirements
-    canvas.height = 450;
-
-    // Get the 2D rendering context
-    const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!;
-    this.drawCircle(ctx, canvas, canvas.height);
-
-    this.drawFillColor(ctx, canvas, 90, 100, 'red');
-    this.drawFillColor(ctx, canvas, 100, 130, 'green');
-    this.drawFillColor(ctx, canvas, 130, 150, 'yellow');
-    this.drawFillColor(ctx, canvas, 150, 210, 'blue');
-    this.drawFillColor(ctx, canvas, 210, 230);
-    this.drawFillColor(ctx, canvas, 230, 260, 'orange');
-    this.drawFillColor(ctx, canvas, 260, 270, 'cyan');
-    this.drawCircle(ctx, canvas, canvas.height - 100);
-
-    this.drawLine(ctx, canvas, 100);
-    this.drawLine(ctx, canvas, 130);
-    this.drawLine(ctx, canvas, 150);
-    this.drawLine(ctx, canvas, 210);
-    this.drawLine(ctx, canvas, 230);
-    this.drawLine(ctx, canvas, 260);
-
-    const dataURL = canvas.toDataURL();
-
-    const imageDiv = document.createElement('img');
-    imageDiv.src = dataURL;
-
-    document.body.appendChild(imageDiv);
-  }
-
-  drawCircle(
-    ctx: CanvasRenderingContext2D,
-    canvas: HTMLCanvasElement,
-    radius: number,
-    strokeStyleColor: string = 'black'
-  ): void {
-    // Set the border color
-    ctx.strokeStyle = strokeStyleColor;
-    // Set the fill color
-    ctx.fillStyle = 'white';
-    // Draw the half circle
-    ctx.beginPath();
-    ctx.arc(canvas.width / 2, canvas.height, radius, 0, Math.PI, true);
-    ctx.lineTo(0, canvas.height);
-    ctx.closePath();
-    // Fill the half circle with white
-    ctx.fill();
-    // Draw the border
-    ctx.stroke();
-  }
-
-  drawFillColor(
-    ctx: CanvasRenderingContext2D,
-    canvas: HTMLCanvasElement,
-    startAngle: number,
-    endAngle: number,
-    color: string = 'white'
-  ): void {
-    // Set the fill color to red
-    ctx.fillStyle = color;
-
-    // Draw the filled area between start angle  and end angle
-    ctx.beginPath();
-    ctx.moveTo(canvas.width / 2, canvas.height);
-    ctx.arc(
-      canvas.width / 2,
-      canvas.height,
-      canvas.height,
-      ((360 - (startAngle - 90)) * Math.PI) / 180,
-      ((360 - (endAngle - 90)) * Math.PI) / 180,
-      true
-    );
-    ctx.closePath();
-
-    // Fill the area with color
-    ctx.fill();
-
-    // Draw the border
-    ctx.stroke();
-  }
-
-  drawLine(
-    ctx: CanvasRenderingContext2D,
-    canvas: HTMLCanvasElement,
-    angle: number,
-    strokeStyleColor: string = 'black'
-  ) {
-    // Calculate the angle for the radius line (30 degrees)
-    const radiusAngle1 = (angle * Math.PI) / 180;
-
-    // Calculate the coordinates for the endpoint of the radius line
-    const endPointX1 =
-      canvas.width / 2 + Math.sin(radiusAngle1) * canvas.height;
-    const endPointY1 = canvas.height + Math.cos(radiusAngle1) * canvas.height;
-
-    // Draw the radius line
-    ctx.beginPath();
-    ctx.moveTo(canvas.width / 2, canvas.height);
-    ctx.lineTo(endPointX1, endPointY1);
-    ctx.closePath();
-
-    // Set the color for the radius line
-    ctx.strokeStyle = strokeStyleColor; // Adjust the color as desired
-
-    // Draw the border for the radius line
-    ctx.stroke();
   }
 }
--- src/app/draw-defect-circle-image.service.ts
+++ src/app/draw-defect-circle-image.service.ts
@@ -4,28 +4,33 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class DrawDefectCircleImageService {
-  private width: number = 900;
-  private height: number = 450;
+  private width: number = 600;
+  private height: number = 400;
+  private centerX!: number;
+  private centerY!: number;
 
   constructor() {}
 
   generateImage() {
     const canvas = document.createElement('canvas');
     canvas.width = 900; // Adjust the dimensions as per your requirements
-    canvas.height = 450;
+    canvas.height = 600;
+
+    this.centerX = canvas.width / 2;
+    this.centerY = canvas.height - 30;
 
     // Get the 2D rendering context
     const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!;
     this.drawCircle(ctx, canvas, this.height);
-
     this.drawFillColor(ctx, canvas, 90, 100, 'red');
     this.drawFillColor(ctx, canvas, 100, 130, 'green');
     this.drawFillColor(ctx, canvas, 130, 150, 'yellow');
     this.drawFillColor(ctx, canvas, 150, 210, 'blue');
-    this.drawFillColor(ctx, canvas, 210, 230);
+    this.drawFillColor(ctx, canvas, 210, 230, 'yellow');
     this.drawFillColor(ctx, canvas, 230, 260, 'orange');
     this.drawFillColor(ctx, canvas, 260, 270, 'cyan');
-    this.drawCircle(ctx, canvas, this.height - 100);
+
+    this.drawCircle(ctx, canvas, this.height - 75);
 
     this.drawLine(ctx, canvas, 100);
     this.drawLine(ctx, canvas, 130);
@@ -33,6 +38,15 @@ export class DrawDefectCircleImageService {
     this.drawLine(ctx, canvas, 210);
     this.drawLine(ctx, canvas, 230);
     this.drawLine(ctx, canvas, 260);
+
+    this.drawText(ctx, 'Posizione Trasversale', this.centerX, 75, 25);
+    this.drawText(ctx, 'CH', this.centerX, 160, 20);
+    this.drawText(ctx, 'RS1', this.centerX * 0.36, 260, 20);
+    this.drawText(ctx, 'RD1', this.centerX * 1.65, 260, 20);
+    this.drawText(ctx, 'RS', this.centerX * 0.13, 400, 20);
+    this.drawText(ctx, 'RD', this.centerX * 1.86, 400, 20);
+    this.drawText(ctx, 'PS', this.centerX * 0.06, 550, 20);
+    this.drawText(ctx, 'RS', this.centerX * 1.95, 550, 20);
 
     const dataURL = canvas.toDataURL();
 
@@ -51,8 +65,8 @@ export class DrawDefectCircleImageService {
     ctx.fillStyle = 'white';
     // Draw the half circle
     ctx.beginPath();
-    ctx.arc(this.width / 2, this.height, radius, 0, Math.PI, true);
-    ctx.lineTo(0, this.height);
+    ctx.arc(this.centerX, this.centerY, radius, 0, Math.PI, true);
+
     ctx.closePath();
     // Fill the half circle with white
     ctx.fill();
@@ -72,10 +86,10 @@ export class DrawDefectCircleImageService {
 
     // Draw the filled area between start angle  and end angle
     ctx.beginPath();
-    ctx.moveTo(this.width / 2, this.height);
+    ctx.moveTo(this.centerX, this.centerY);
     ctx.arc(
-      this.width / 2,
-      this.height,
+      this.centerX,
+      this.centerY,
       this.height,
       ((360 - (startAngle - 90)) * Math.PI) / 180,
       ((360 - (endAngle - 90)) * Math.PI) / 180,
@@ -100,12 +114,12 @@ export class DrawDefectCircleImageService {
     const radiusAngle1 = (angle * Math.PI) / 180;
 
     // Calculate the coordinates for the endpoint of the radius line
-    const endPointX1 = this.width / 2 + Math.sin(radiusAngle1) * this.height;
-    const endPointY1 = this.height + Math.cos(radiusAngle1) * this.height;
+    const endPointX1 = this.centerX + Math.sin(radiusAngle1) * this.height;
+    const endPointY1 = this.centerY + Math.cos(radiusAngle1) * this.height;
 
     // Draw the radius line
     ctx.beginPath();
-    ctx.moveTo(this.width / 2, this.height);
+    ctx.moveTo(this.centerX, this.centerY);
     ctx.lineTo(endPointX1, endPointY1);
     ctx.closePath();
 
@@ -114,5 +128,18 @@ export class DrawDefectCircleImageService {
 
     // Draw the border for the radius line
     ctx.stroke();
+  }
+
+  private drawText(
+    ctx: CanvasRenderingContext2D,
+    text: string,
+    startX: number,
+    startY: number,
+    fontSize: number = 30
+  ): void {
+    ctx.fillStyle = 'black';
+    ctx.font = `bold ${fontSize}px Arial`;
+    ctx.textAlign = 'center';
+    ctx.fillText(text, startX, startY);
   }
 }
