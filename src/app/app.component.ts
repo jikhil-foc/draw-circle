2d12e7b7d4440e7152e5f1ffd34edbf9c24c7576
Jikhil Ps <jikhilps@focaloid.com>
2023-06-15 15:47:41 +0530

    rectangle shape added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,64 @@ export class AppComponent {
     const parentDiv = document.createElement('div');
 
     [1].forEach(() => {
-      const data = this.drawDefectService.generateCircleImage();
+      const s1 = {
+        PS: true,
+        RS: false,
+        RS1: true,
+        CH: false,
+        RD1: false,
+        RD: false,
+        PD: true,
+      };
+
+      const s2 = {
+        PS: false,
+        RS: true,
+        RS1: false,
+        CH: false,
+        RD1: true,
+        RD: false,
+        PD: false,
+      };
+
+      const s3 = {
+        PS: true,
+        RS: false,
+        RS1: false,
+        CH: true,
+        RD1: false,
+        RD: true,
+        PD: false,
+      };
+
+      const s4 = {
+        PS: false,
+        RS: true,
+        RS1: false,
+        CH: true,
+        RD1: false,
+        RD: true,
+        PD: true,
+      };
+      const arr = [s1, s2, s3, s4];
+
+      const data = this.drawDefectService.generateRectangleImage(arr);
+      const imageDiv = document.createElement('img');
+      imageDiv.src = data;
+      document.body.appendChild(imageDiv);
+    });
+
+    [1].forEach(() => {
+      const ss = {
+        PS: true,
+        RS: true,
+        RS1: true,
+        CH: true,
+        RD1: true,
+        RD: true,
+        PD: true,
+      };
+      const data = this.drawDefectService.generateCircleImage(ss);
       const imageDiv = document.createElement('img');
       imageDiv.src = data;
       document.body.appendChild(imageDiv);
--- src/app/draw-defect-circle-image.service.ts
+++ src/app/draw-defect-circle-image.service.ts
@@ -1,39 +1,128 @@
 import { Injectable } from '@angular/core';
 
+interface SezioneLongitudinale {
+  PS: boolean;
+  RS: boolean;
+  RS1: boolean;
+  CH: boolean;
+  RD1: boolean;
+  RD: boolean;
+  PD: boolean;
+}
+
+enum SezioneLongitudinaleColor {
+  PS = '#2f75b5',
+  RS = '#26944a',
+  RS1 = '#fee500',
+  CH = '#d93628',
+  RD1 = '#fee500',
+  RD = '#26944a',
+  PD = '#2f75b5',
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class DrawDefectCircleImageService {
   constructor() {}
 
-  generateCircleImage() {
+  generateCircleImage(sezioneLongitudinale: SezioneLongitudinale) {
     const canvas = document.createElement('canvas');
     canvas.width = 900; // Adjust the dimensions as per your requirements
     canvas.height = 600;
     let imageHeight: number = 400;
-
     let centerX = canvas.width / 2;
     let centerY = canvas.height - 30;
 
     // Get the 2D rendering context
     const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!;
     this.drawHalfCircle(ctx, imageHeight, centerX, centerY);
-    this.drawFillColor(ctx, 90, 100, centerX, centerY, imageHeight, 'red');
-    this.drawFillColor(ctx, 100, 130, centerX, centerY, imageHeight, 'green');
-    this.drawFillColor(ctx, 130, 150, centerX, centerY, imageHeight, 'yellow');
-    this.drawFillColor(ctx, 150, 210, centerX, centerY, imageHeight, 'blue');
-    this.drawFillColor(ctx, 210, 230, centerX, centerY, imageHeight, 'yellow');
-    this.drawFillColor(ctx, 230, 260, centerX, centerY, imageHeight, 'orange');
-    this.drawFillColor(ctx, 260, 270, centerX, centerY, imageHeight, 'cyan');
+
+    // PD
+    this.drawFillColorInsideCircle(
+      ctx,
+      90,
+      100,
+      centerX,
+      centerY,
+      imageHeight,
+      sezioneLongitudinale.PD ? SezioneLongitudinaleColor.PD : undefined
+    );
+
+    // RD
+    this.drawFillColorInsideCircle(
+      ctx,
+      100,
+      130,
+      centerX,
+      centerY,
+      imageHeight,
+      sezioneLongitudinale.RD ? SezioneLongitudinaleColor.RD : undefined
+    );
+
+    // RD1
+    this.drawFillColorInsideCircle(
+      ctx,
+      130,
+      150,
+      centerX,
+      centerY,
+      imageHeight,
+      sezioneLongitudinale.RD1 ? SezioneLongitudinaleColor.RD1 : undefined
+    );
+
+    // CH
+    this.drawFillColorInsideCircle(
+      ctx,
+      150,
+      210,
+      centerX,
+      centerY,
+      imageHeight,
+      sezioneLongitudinale.CH ? SezioneLongitudinaleColor.CH : undefined
+    );
+
+    // RS1
+    this.drawFillColorInsideCircle(
+      ctx,
+      210,
+      230,
+      centerX,
+      centerY,
+      imageHeight,
+      sezioneLongitudinale.RS1 ? SezioneLongitudinaleColor.RS1 : undefined
+    );
+
+    // RS
+    this.drawFillColorInsideCircle(
+      ctx,
+      230,
+      260,
+      centerX,
+      centerY,
+      imageHeight,
+      sezioneLongitudinale.RS ? SezioneLongitudinaleColor.RS : undefined
+    );
+
+    // PS
+    this.drawFillColorInsideCircle(
+      ctx,
+      260,
+      270,
+      centerX,
+      centerY,
+      imageHeight,
+      sezioneLongitudinale.PS ? SezioneLongitudinaleColor.PS : undefined
+    );
 
     this.drawHalfCircle(ctx, imageHeight - 75, centerX, centerY);
 
-    this.drawLine(ctx, 100, centerX, centerY, imageHeight);
-    this.drawLine(ctx, 130, centerX, centerY, imageHeight);
-    this.drawLine(ctx, 150, centerX, centerY, imageHeight);
-    this.drawLine(ctx, 210, centerX, centerY, imageHeight);
-    this.drawLine(ctx, 230, centerX, centerY, imageHeight);
-    this.drawLine(ctx, 260, centerX, centerY, imageHeight);
+    this.drawLineInsideCircle(ctx, 100, centerX, centerY, imageHeight);
+    this.drawLineInsideCircle(ctx, 130, centerX, centerY, imageHeight);
+    this.drawLineInsideCircle(ctx, 150, centerX, centerY, imageHeight);
+    this.drawLineInsideCircle(ctx, 210, centerX, centerY, imageHeight);
+    this.drawLineInsideCircle(ctx, 230, centerX, centerY, imageHeight);
+    this.drawLineInsideCircle(ctx, 260, centerX, centerY, imageHeight);
 
     this.drawText(ctx, 'Posizione Trasversale', centerX, 75, 25);
     this.drawText(ctx, 'CH', centerX, 160, 20);
@@ -42,10 +131,44 @@ export class DrawDefectCircleImageService {
     this.drawText(ctx, 'RS', centerX * 0.13, 400, 20);
     this.drawText(ctx, 'RD', centerX * 1.86, 400, 20);
     this.drawText(ctx, 'PS', centerX * 0.06, 550, 20);
-    this.drawText(ctx, 'RS', centerX * 1.95, 550, 20);
+
+    this.drawText(ctx, 'PD', centerX * 1.95, 550, 20);
 
     const dataURL = canvas.toDataURL();
 
+    return dataURL;
+  }
+
+  generateRectangleImage(sezioneLongitudinaleList: SezioneLongitudinale[]) {
+    const canvas = document.createElement('canvas');
+    canvas.width = 900; // Adjust the dimensions as per your requirements
+    canvas.height = 700;
+
+    // Calculate the center coordinates
+    let centerX = canvas.width / 2;
+    let centerY = canvas.height / 2;
+    // Get the 2D rendering context
+    const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!;
+
+    this.drawRectangular(ctx, centerX, centerY, sezioneLongitudinaleList);
+
+    const cellWidth = centerX / 4;
+    const cellHeight = centerY / 7;
+    this.drawText(ctx, 'Posizione Longitudinale', centerX, 75, 25);
+    this.drawText(ctx, '0-5', centerX - 1.5 * cellWidth, 150, 18);
+    this.drawText(ctx, '5-10', centerX - 0.5 * cellWidth, 150, 18);
+    this.drawText(ctx, '10-15', centerX + 0.5 * cellWidth, 150, 18);
+    this.drawText(ctx, '15-20', centerX + 1.5 * cellWidth, 150, 18);
+
+    this.drawText(ctx, 'PS', 180, centerY - 2.9 * cellHeight, 18, 'left');
+    this.drawText(ctx, 'RS', 180, centerY - 1.9 * cellHeight, 18, 'left');
+    this.drawText(ctx, 'RS1', 180, centerY - 0.9 * cellHeight, 18, 'left');
+    this.drawText(ctx, 'CH', 180, centerY + 0.2 * cellHeight, 18, 'left');
+    this.drawText(ctx, 'RD1', 180, centerY + 1.1 * cellHeight, 18, 'left');
+    this.drawText(ctx, 'RD', 180, centerY + 2.1 * cellHeight, 18, 'left');
+    this.drawText(ctx, 'PD', 180, centerY + 3.1 * cellHeight, 18, 'left');
+
+    const dataURL = canvas.toDataURL();
     return dataURL;
   }
 
@@ -60,6 +183,7 @@ export class DrawDefectCircleImageService {
     ctx.strokeStyle = strokeStyleColor;
     // Set the fill color
     ctx.fillStyle = 'white';
+
     // Draw the half circle
     ctx.beginPath();
     ctx.arc(centerX, centerY, radius, 0, Math.PI, true);
@@ -71,18 +195,20 @@ export class DrawDefectCircleImageService {
     ctx.stroke();
   }
 
-  private drawFillColor(
+  private drawFillColorInsideCircle(
     ctx: CanvasRenderingContext2D,
     startAngle: number,
     endAngle: number,
     centerX: number,
     centerY: number,
     height: number,
-    color: string = 'white'
+    color: string = 'white',
+    strokeStyle: string = 'black'
   ): void {
     // Set the fill color to red
     ctx.fillStyle = color;
-
+    ctx.strokeStyle = strokeStyle;
+    ctx.lineWidth = 2;
     // Draw the filled area between start angle  and end angle
     ctx.beginPath();
     ctx.moveTo(centerX, centerY);
@@ -103,12 +229,13 @@ export class DrawDefectCircleImageService {
     ctx.stroke();
   }
 
-  private drawLine(
+  private drawLineInsideCircle(
     ctx: CanvasRenderingContext2D,
     angle: number,
     centerX: number,
     centerY: number,
     height: number,
+    addDottedLine: boolean = false,
     strokeStyleColor: string = 'black'
   ) {
     // Calculate the angle for the radius line (30 degrees)
@@ -117,6 +244,10 @@ export class DrawDefectCircleImageService {
     // Calculate the coordinates for the endpoint of the radius line
     const endPointX = centerX + Math.sin(radiusAngle) * height;
     const endPointY = centerY + Math.cos(radiusAngle) * height;
+
+    if (addDottedLine) {
+      ctx.setLineDash([10, 10]); // Set the dash pattern (5 pixels on, 5 pixels off)
+    }
 
     // Draw the radius line
     ctx.beginPath();
@@ -136,11 +267,56 @@ export class DrawDefectCircleImageService {
     text: string,
     startX: number,
     startY: number,
-    fontSize: number = 30
+    fontSize: number = 30,
+    textAlign: CanvasTextAlign = 'center'
   ): void {
     ctx.fillStyle = 'black';
     ctx.font = `bold ${fontSize}px Arial`;
-    ctx.textAlign = 'center';
+    ctx.textAlign = textAlign;
     ctx.fillText(text, startX, startY);
+  }
+
+  private drawRectangular(
+    ctx: CanvasRenderingContext2D,
+    centerX: number,
+    centerY: number,
+    sezioneLongitudinaleList: SezioneLongitudinale[]
+  ) {
+    // Calculate the width and height of each cell
+    const cellWidth = centerX / 4;
+    const cellHeight = centerY / 7;
+
+    // Calculate the starting point of the rectangle
+    const startX = centerX - (cellWidth * 4) / 2;
+    const startY = centerY - (cellHeight * 7) / 2;
+
+    // Draw the rectangle using a loop
+
+    Object.keys(sezioneLongitudinaleList[0]).forEach(
+      (item: any, row: number) => {
+        for (let col = 0; col < 4; col++) {
+          // Calculate the coordinates of the current cell
+          const x = startX + col * cellWidth;
+          const y = startY + row * cellHeight;
+
+          const selectedItem: any = sezioneLongitudinaleList[col];
+
+          if (selectedItem[item]) {
+            ctx.fillStyle =
+              SezioneLongitudinaleColor[
+                item as keyof typeof SezioneLongitudinaleColor
+              ] ?? 'white';
+          } else {
+            ctx.fillStyle = 'white';
+          }
+
+          ctx.fillRect(x, y, cellWidth, cellHeight);
+
+          // Set the stroke color and draw the rectangle outline
+          ctx.strokeStyle = 'black';
+          ctx.strokeRect(x, y, cellWidth, cellHeight);
+        }
+      }
+    );
   }
 }
